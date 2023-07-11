98287e805c6ee8512267f6cfa65f3eb83570fca3
Just Vervaart <35161717+justusvervaart@users.noreply.github.com>
2023-07-11 22:05:18 +0200

    Update scrape_ovfiets.py

--- scrape_ovfiets.py
+++ scrape_ovfiets.py
@@ -3,7 +3,6 @@ import json
 import csv
 import datetime
 import time
-import os
 
 # Verkrijg de JSON data
 response = requests.get("http://fiets.openov.nl/locaties.json")
@@ -16,20 +15,25 @@ for key, location in data['locaties'].items():
     location_code = location.get('extra', {}).get('locationCode', '')
     rental_bikes = location.get('extra', {}).get('rentalBikes', '')
     fetch_time = location.get('extra', {}).get('fetchTime', '')
+    loc_type = location.get('extra', {}).get('type', '')
     open_status = location.get('open', '')
-    extracted_data.append([description, location_code, rental_bikes, fetch_time, open_status])
-
-# Maak de map 'scrapes' als deze nog niet bestaat
-os.makedirs('scrapes', exist_ok=True)
+    extracted_data.append([description, location_code, rental_bikes, fetch_time, loc_type, open_status])
 
 # Maak een CSV bestandsnaam met de huidige tijd als Unix Timestamp
 current_time = int(time.time())
-filename = os.path.join(os.getcwd(), 'scrapes', f'beschikbaarheid_ov-fietsen_{current_time}.csv')
+filename = f"beschikbaarheid_ov-fietsen_{current_time}.csv"
 
 # Schrijf de gegevens naar een CSV-bestand
 with __builtins__.open(filename, 'w', newline='') as f:
     writer = csv.writer(f)
-    writer.writerow(["description", "locationCode", "rentalBikes", "fetchTime", "open"])
+    writer.writerow(["description", "locationCode", "rentalBikes", "fetchTime", "type", "open"])
     writer.writerows(extracted_data)
 
 print(f"Data geschreven naar {filename}")
+
+# Voeg de nieuwe gegevens toe aan het 'master' CSV-bestand
+with __builtins__.open("beschikbaarheid_ov-fietsen_master.csv", 'a', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerows(extracted_data)
+
+print("Data toegevoegd aan beschikbaarheid_ov-fietsen_master.csv")
